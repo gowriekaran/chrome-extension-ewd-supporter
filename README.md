5281b9fae4d223b19b834666e6b25275be295783
Gowriekaran Sinnadurai <gowriekaran@users.noreply.github.com>
2022-02-28 00:08:53 -0500

    v2.1

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # EWD Supporter
-Internal Chrome Extension to facilitate troubleshooting for [Etoile Web Design](https://www.etoilewebdesign.com/).
+Internal Chrome Extension to facilitate [Etoile Web Design](https://www.etoilewebdesign.com/) troubleshooting on WordPress Support Forum.
 
 ##### Features
-- Floating bookmarks container that contains all plugin active support thread URLs
-- Color code user handles
-- Auto scroll to threads container
+- Floating bookmarks container that contains all plugins' active support thread URLs as icons and free image hosting website.
+- Automatically highlight non-staff handles in bbp-topic-freshness-author element.
+- Auto scroll to Forum threads container.
+- Option to open all plugins' active thread with the click of a button.
--- extension/assets/js/contentScript.js
+++ extension/assets/js/contentScript.js
@@ -11,8 +11,35 @@ $(document).ready(function () {
         }
     });
 
+    function openInNewTab(links) {
+        for (var x = 0; x < links.length; x++){
+            window.open(links[x], '_blank').focus();
+        }
+    }
+
     function show_ewdSupporter() {
-        $.get(chrome.extension.getURL("assets/html/ewdSupporterBookmarkContainer.html"), function (data) {
+        $(".global-header").hide();
+        $("html").css("margin-top", "0px");
+        $("#main > div.entry-meta.sidebar > div:nth-child(2)").hide();
+        
+        const links = [
+            "https://wordpress.org/support/plugin/ultimate-product-catalogue/active/",
+            "https://wordpress.org/support/plugin/front-end-only-users/active/",
+            "https://wordpress.org/support/plugin/order-tracking/active/",
+            "https://wordpress.org/support/plugin/ultimate-faqs/active/",
+            "https://wordpress.org/support/plugin/ultimate-reviews/active/",
+            "https://wordpress.org/support/plugin/ultimate-slider/active/",
+            "https://wordpress.org/support/plugin/color-filters/active/",
+            "https://wordpress.org/support/plugin/ultimate-lightbox/active/",
+            "https://wordpress.org/support/plugin/ultimate-appointment-scheduling/active/",
+            "https://wordpress.org/support/plugin/ultimate-wp-mail/active/",
+            "https://wordpress.org/support/plugin/business-profile/active/",
+            "https://wordpress.org/support/plugin/food-and-drink-menu/active/",
+            "https://wordpress.org/support/plugin/restaurant-reservations/active/",
+            "https://wordpress.org/support/plugin/good-reviews-wp/active/"
+        ];
+
+        $.get(chrome.extension.getURL("assets/html/ewdSupporterControlsContainer.html"), function (data) {
             $(data).appendTo("body");
             
             if (window.location.href.indexOf("topic") > -1) {
@@ -20,14 +47,21 @@ $(document).ready(function () {
             } else {
                 $(".bbp-topic-permalink").parent().siblings(".bbp-topic-freshness").find(".bbp-author-name").each(function () {
                     if (this.innerHTML != "jaysupport" && this.innerHTML != "jssupport" && this.innerHTML != "Rustaurius") {
-                        $(this).css("color", "#f44336");
+                        $(this).css("color", "red");
+                        $(this).css("font-size", "20px");
+                        $(this).css("background", "black");
                     }
                 });
             }
 
             $('html, body').animate({
-                scrollTop: $(".page-header, .bbp-pagination").offset().top
+                scrollTop: $("#bbpress-forums").offset().top
             }, 500);
+
+            $("#ewdSupporterOpenAllLinks").click(function () {
+                openInNewTab(links);
+            });
+
         });
     }
 
